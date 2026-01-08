56907e7d294d9758675839aa32ac113504d584a3
Firmansyah Romadhoni <119273705+firmansyahr@users.noreply.github.com>
2026-01-08 13:15:28 +0700

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 @st.cache_data
 def load_data():
-    file_path = "data/Purchasing_Dataset.xlsx"
+    file_path = "data/FMCG_Purchasing_Dataset.xlsx"
 
     po = pd.read_excel(file_path, sheet_name="Purchase_Order", parse_dates=["po_date","expected_delivery_date"])
     gr = pd.read_excel(file_path, sheet_name="Goods_Receipt", parse_dates=["gr_date"])
