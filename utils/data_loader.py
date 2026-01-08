96d6bf489a465f21c334643d6904f76064aed6e8
Firmansyah Romadhoni <119273705+firmansyahr@users.noreply.github.com>
2026-01-08 11:50:25 +0700

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -4,11 +4,10 @@ import streamlit as st
 @st.cache_data
 def load_data():
     file_path = "data/Purchasing_Dataset.xlsx"
-    
-    po = pd.read_excel(file_path, sheet_name="Purchase_Order")
-    gr = pd.read_excel(file_path, sheet_name="Goods_Receipt")
-    inv = pd.read_excel(file_path, sheet_name="Inventory")
-    cons = pd.read_excel(file_path, sheet_name="Material_Consumption")
-    sup = pd.read_excel(file_path, sheet_name="Supplier_Performance")
 
-    return po, gr, inv, cons, sup
+    po = pd.read_excel(file_path, sheet_name="Purchase_Order", parse_dates=["po_date","expected_delivery_date"])
+    gr = pd.read_excel(file_path, sheet_name="Goods_Receipt", parse_dates=["gr_date"])
+    inv = pd.read_excel(file_path, sheet_name="Inventory", parse_dates=["date"])
+    cons = pd.read_excel(file_path, sheet_name="Material_Consumption", parse_dates=["production_date"])
+
+    return po, gr, inv, cons
